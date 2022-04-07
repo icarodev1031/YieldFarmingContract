3888f7ac9a0ed10f9abc1628d145e3eeb9565682
icarodev1031 <icarodev1031@gmail.com>
2022-04-08 05:16:06 +0800

    PmknFarm.sol and PmknToken.sol complete

---
+++ contracts/PmknToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PmknToken is ERC20, Ownable{
+    constructor() ERC20("PmknToken","PMKN"){
+
+    }
+    function mint(address to, uint256 amount) public onlyOwner{
+        _mint(to, amount);
+    }
+}
